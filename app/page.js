d7570b959dc840206d4c86e665eb10e2f92e805c
limitnl <hkrootyt@gmail.com>
2024-11-22 18:53:15 -0600

    add unfinished navbar & footer

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "./Navbar"
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <header> <Navbar/> </header>
       <main className={styles.main}>
         <Image
           className={styles.logo}
@@ -47,48 +49,15 @@ export default function Home() {
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <a href="/page.js" className="a-footer"><Image
+      aria-hidden
+      src="/footerlogo.svg"
+      alt="Footer Logo"
+      width={75}
+      height={75}
+      />
+      </a>
+      <span>portfolio voor limitnl</span>
       </footer>
     </div>
   );
